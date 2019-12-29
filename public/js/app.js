5fa7602f2d8e9a39de0eb14b754188cd70e2a5ca
Niklas Stambor <ni.stambor@gmail.com>
2019-12-29 02:20:28 +0100

    rebuild assets

--- public/js/app.js
+++ public/js/app.js
@@ -49613,8 +49613,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/niklas/projects/gitlab.com/N_klas/laravel-tailwind-template/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/niklas/projects/gitlab.com/N_klas/laravel-tailwind-template/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/niklas/projects/gitlab.com/N_klas/xec/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/niklas/projects/gitlab.com/N_klas/xec/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
